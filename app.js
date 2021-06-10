2517e9f34ff0397744b706ed10c269d507f20183
thrvg <thrvg1@gmail.com>
2021-06-10 16:51:43 -0400

    Static fix 6

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+//app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 //app.use('/static', express.static('public'))
 //app.use(express.static(path.join(__dirname, 'public')));
 //app.use('/static', express.static(path.join(__dirname, 'public')));
